73d9d2c662877c1fc01a7d063b9e967a34bcf055
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-05 12:23:17 +0000

    Reorder projects in groups [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- prod/run.sh
+++ prod/run.sh
@@ -9,6 +9,6 @@ helm install devstats-prod-patroni ./devstats-helm --set skipSecrets=1,skipPVs=1
 helm install devstats-prod-statics ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipCrons=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,indexStaticsFrom=1 || exit 1
 helm install devstats-prod-ingress ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipCrons=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipStatic=1,indexDomainsFrom=1,ingressClass=nginx-prod || exit 1
 helm install devstats-prod-bootstrap ./devstats-helm --set skipSecrets=1,skipPVs=1,skipProvisions=1,skipCrons=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,skipStatic=1 || exit 1
-helm install devstats-prod-crons ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,skipStatic=1,indexCronsFrom=46,indexCronsTo=51 || exit 1
-helm install devstats-prod-grafanas ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipCrons=1,skipPostgres=1,skipIngress=1,skipStatic=1,indexGrafanasFrom=46,indexGrafanasTo=51,indexServicesFrom=46,indexServicesTo=51 || exit 1
+helm install devstats-prod-crons ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,skipStatic=1,indexCronsFrom=44,indexCronsTo=49 || exit 1
+helm install devstats-prod-grafanas ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipCrons=1,skipPostgres=1,skipIngress=1,skipStatic=1,indexGrafanasFrom=44,indexGrafanasTo=49,indexServicesFrom=44,indexServicesTo=49 || exit 1
 helm list || exit 1
