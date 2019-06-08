0b3e220710c22277a176e7ae9c54b89e417a7d26
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-08 08:52:08 +0000

    Different cron schedule for test and prod namespaces [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- prod/run.sh
+++ prod/run.sh
@@ -11,5 +11,5 @@ helm install devstats-prod-ingress ./devstats-helm --set skipSecrets=1,skipPVs=1
 helm install devstats-prod-bootstrap ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBackupsPV=1,skipBackups=1,skipProvisions=1,skipCrons=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,skipStatic=1 || exit 1
 helm install devstats-prod-crons ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBackupsPV=1,skipBackups=1,skipBootstrap=1,skipProvisions=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,skipStatic=1,indexCronsFrom=44,indexCronsTo=49 || exit 1
 helm install devstats-prod-grafanas ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBackupsPV=1,skipBackups=1,skipBootstrap=1,skipProvisions=1,skipCrons=1,skipPostgres=1,skipIngress=1,skipStatic=1,indexGrafanasFrom=44,indexGrafanasTo=49,indexServicesFrom=44,indexServicesTo=49 || exit 1
-helm install devstats-prod-backups ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipCrons=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,skipStatic=1 || exit 1
+helm install devstats-prod-backups ./devstats-helm --set skipSecrets=1,skipPVs=1,skipBootstrap=1,skipProvisions=1,skipCrons=1,skipGrafanas=1,skipServices=1,skipPostgres=1,skipIngress=1,skipStatic=1,backupsCron='0 3 * * *' || exit 1
 helm list || exit 1
