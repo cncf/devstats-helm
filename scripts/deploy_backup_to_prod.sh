61a92796a0c993ce1a6524f1a3a0278d8cf5f7b4
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-12-16 06:29:07 +0000

    Deploy prod projects
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ scripts/deploy_backup_to_prod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: please provide the project name as a 1st arg"
+  exit 1
+fi
+if [ -z "$2" ]
+then
+  echo "$0: please provide index from as a 2nd arg"
+  exit 2
+fi
+if [ -z "$3" ]
+then
+  echo "$0: please provide index to as a 3rd arg"
+  exit 3
+fi
+echo helm install "devstats-prod-${1}" ./devstats-helm --set "namespace='devstats-prod',skipSecrets=1,skipBackupsPV=1,skipVacuum=1,skipBackups=1,skipBootstrap=1,skipPostgres=1,skipIngress=1,skipStatic=1,skipAPI=1,skipNamespaces=1,indexPVsFrom=$2,indexPVsTo=$3,indexProvisionsFrom=$2,indexProvisionsTo=$3,indexCronsFrom=$2,indexCronsTo=$3,indexGrafanasFrom=$2,indexGrafanasTo=$3,indexServicesFrom=$2,indexServicesTo=$3,indexAffiliationsFrom=$2,indexAffiliationsTo=$3,provisionImage='lukaszgryglicki/devstats-prod',provisionCommand='devstats-helm/restore.sh',restoreFrom='https://devstats.cncf.io/backups/'"
+#helm install "devstats-prod-${1}" ./devstats-helm --set namespace='devstats-prod',skipSecrets=1,skipBackupsPV=1,skipVacuum=1,skipBackups=1,skipBootstrap=1,skipPostgres=1,skipIngress=1,skipStatic=1,skipAPI=1,skipNamespaces=1,indexPVsFrom=$2,indexPVsTo=$3,indexProvisionsFrom=$2,indexProvisionsTo=$3,indexCronsFrom=$2,indexCronsTo=$3,indexGrafanasFrom=$2,indexGrafanasTo=$3,indexServicesFrom=$2,indexServicesTo=$3,indexAffiliationsFrom=$2,indexAffiliationsTo=$3,provisionImage='lukaszgryglicki/devstats-prod',provisionCommand='devstats-helm/restore.sh',restoreFrom='https://devstats.cncf.io/backups/'
+echo "clear; k logs -f devstats-provision-${1}"
