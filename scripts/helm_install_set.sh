ce55ba77af1e546df19bd0bd3b5742b2f2cfaf3f
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-12-22 16:45:04 +0100

    Fix the script
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/helm_install_set.sh
+++ scripts/helm_install_set.sh
@@ -14,4 +14,4 @@ then
   echo "please provide index to as a 3rd arg"
   exit 3
 fi
-helm install "$1" ./devstats-helm --set namespace='devstats-prod',skipSecrets=1,skipPVs=1,skipBackupsPV=1,skipVacuum=1,skipBackups=1,skipBootstrap=1,indexProvisionsFrom=$2,indexProvisionsTo=$3,indexCronsFrom=$2,indexCronsTo=$3,indexGrafanasFrom=$2,indexGrafanasTo=$3,indexServicesFrom=$2,indexServicesTo=$3,indexAffiliationsFrom=51,indexAffiliationsTo=100,^CipPostgres=1,skipIngress=1,skipStatic=1,skipAPI=1,skipNamespaces=1,testServer='',prodServer='1',skipECFRGReset=1,nCPUs=2,skipAddAll=1,allowMetricFail=1
+helm install "$1" ./devstats-helm --set namespace='devstats-prod',skipSecrets=1,skipPVs=1,skipBackupsPV=1,skipVacuum=1,skipBackups=1,skipBootstrap=1,indexProvisionsFrom=$2,indexProvisionsTo=$3,indexCronsFrom=$2,indexCronsTo=$3,indexGrafanasFrom=$2,indexGrafanasTo=$3,indexServicesFrom=$2,indexServicesTo=$3,indexAffiliationsFrom=$2,indexAffiliationsTo=$3,skipPostgres=1,skipIngress=1,skipStatic=1,skipAPI=1,skipNamespaces=1,testServer='',prodServer='1',skipECFRGReset=1,nCPUs=2,skipAddAll=1,allowMetricFail=1
